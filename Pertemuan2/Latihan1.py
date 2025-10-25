6df14fcc9464faab6a9472c60870ddbde295fee4
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-10-25 11:02:10 +0700

    Update Latihan1.py

--- Pertemuan2/Latihan1.py
+++ Pertemuan2/Latihan1.py
@@ -6,3 +6,9 @@ volume = panjang * lebar * tinggi
 
 print("Hasil Volume Balok Adalah")
 print("Volume Kubik adalah ", volume, "meter³")
+
+Masukkan panjang balok (m): 20
+Masukkan lebar balok (m): 10
+Masukkan tinggi balok (m): 25
+Hasil Volume Balok Adalah
+Volume Kubik adalah  5000 meter³
