533e0808a24566907f822f6b182c9a88de1b0f99
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-10-25 11:01:07 +0700

    Update Latihan1.py

--- Pertemuan2/Latihan1.py
+++ Pertemuan2/Latihan1.py
@@ -1,1 +1,8 @@
+panjang = int(input("Masukkan panjang balok (m): "))
+lebar = int(input("Masukkan lebar balok (m): "))
+tinggi = int(input("Masukkan tinggi balok (m): "))
 
+volume = panjang * lebar * tinggi
+
+print("Hasil Volume Balok Adalah")
+print("Volume Kubik adalah ", volume, "meter³")
