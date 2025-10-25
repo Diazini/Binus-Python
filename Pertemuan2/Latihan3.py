4b162928c46a024650bc8b4ccb6d924c52bb1d5b
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-10-25 11:50:02 +0700

    Create Latihan3.py

---
+++ Pertemuan2/Latihan3.py
@@ -0,0 +1,6 @@
+jari_jari = int(input("Masukkan jari-jari tabung (meter): "))
+tinggi = int(input("Masukkan tinggi tabung (meter): "))
+
+volume = 22 / 7 * (jari_jari * jari_jari) * tinggi
+
+print("Volume tabung adalah", volume, "meter³")
