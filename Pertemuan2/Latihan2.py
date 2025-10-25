416efbe0d601fc0f9d3ac5d537bcdc2de32b35a2
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-10-25 11:26:34 +0700

    Update Latihan2.py

--- Pertemuan2/Latihan2.py
+++ Pertemuan2/Latihan2.py
@@ -1,1 +1,6 @@
+jari_jari = int(input("Masukan Jari_jari (m): "))
 
+volume = (4 / 3) * 22 / 7 * (jari_jari ** 3)
+
+print("Hasil dari jari_jari adalah")
+print("Volume Bola adalah", volume , "meter³")
