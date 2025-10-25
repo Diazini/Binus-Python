ae788b8195ed8bb77de86114d56d368607ef694b
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-10-25 12:00:40 +0700

    Update Latihan4.py

--- Pertemuan2/Latihan4.py
+++ Pertemuan2/Latihan4.py
@@ -1,1 +1,7 @@
+a = int(input("Masukkan nilai A: "))
+b = int(input("Masukkan nilai B: "))
+c = int(input("Masukkan nilai C: "))
 
+diskriminan = (b**2) - (4 *a * c)
+
+print(f"Nilai diskriminan adalah: {diskriminan}")
