52d45e29f6950f79ebab5095eaeb73a49a7d7e2e
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-12-05 14:20:17 +0700

    Create 2.py

---
+++ JawabanSoalAlgoritmaDanPemogramanDasar/2.py
@@ -0,0 +1,21 @@
+A. Variabel adalah tempat untuk menyimpan data atau nilai yang dapat
+berubah-ubah selama program berjalan. Variabel diberi nama tertentu agar niali bisa dipanggil dan digunakan dalam operasi berikutnya.
+
+B.  Variabel pada program = 
+- y2h_ 
+- _t
+- x
+
+
+C. y2h = 0
+_t = 18
+
+for x in range(5):
+    if x % 2 == 1:
+        y2h = _t - x
+    elif x != 0 and _t > x:
+        _t = _t // x
+    else:
+        y2h = y2h + 1
+
+print(x, y2h)
