50e5f1050d9d872a35f154fb1db42b6bb08dad0e
MDIAZ <123314088+Diazini@users.noreply.github.com>
2025-12-05 14:09:44 +0700

    Update 1.py

--- JawabanSoalAlgoritmaDanPemogramanDasar/1.py
+++ JawabanSoalAlgoritmaDanPemogramanDasar/1.py
@@ -1,1 +1,7 @@
+a = float(input("Masukkan nilai a: "))
+b = float(input("Masukkan nilai b: "))
+v = float(input("Masukkan nilai v: "))
 
+y = (b - 10) / (2 * (1 - (a / (2 * v**2))))
+
+print("Hasil y =", y)
